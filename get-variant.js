fe5c1e46ec28796b6dbdf9effab84e275974b060
shobhitmehra82 <shobhit.mehra@vgroup.net>
2026-09-07 11:32:05 +0530

    delete

--- get-variant.js
+++
@@ -1,8 +0,0 @@
-const { adminRequest } = require("./adminClient");
-adminRequest(`
-query {
-products(first: 1) {
-edges { node { title variants(first: 1) { edges { node { id } } } } }
-}
-}
-`).then((data) => console.log(JSON.stringify(data, null, 2))).catch(console.error);
